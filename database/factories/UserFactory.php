aea3bd93870f22c6882931d3536aa1c36ae5c1d3
david <david-github@latotzky.de>
2018-04-01 00:47:25 +0200

    fix factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,7 +13,7 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(Sitetpl\User::class, function (Faker $faker) {
+$factory->define(Sitetpl\Models\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
