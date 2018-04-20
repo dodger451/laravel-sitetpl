438786c1aa3572018c1fa4decc0c3275c6db6bc7
david <david-github@latotzky.de>
2018-04-20 23:11:52 +0200

    Cleanup CodequalityProvider

--- app/Providers/CodequalityProvider.php
+++ app/Providers/CodequalityProvider.php
@@ -1,6 +1,5 @@
 <?php
 namespace Sitetpl\Providers;
-use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 
 	/**
@@ -14,48 +13,21 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 class CodequalityProvider extends ServiceProvider
 {
 	/**
-	 * This namespace is applied to your controller routes.
-	 *
-	 * In addition, it is set as the URL generator's root namespace.
-	 *
-	 * @var string
-	 */
-	protected $namespace = 'App\Http\Controllers';
-	/**
-	 * Define your route model bindings, pattern filters, etc.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		parent::boot();
-	}
-	/**
-	 * Define the routes for the application.
+	 * Define the commands for the application.
 	 *
 	 * @return void
 	 */
 	public function map()
 	{
-		$this->mapConsoleRoutes();
-	}
-	/**
-	 * Define the "console" routes for the application.
-	 *
-	 * These routes are typically stateless.
-	 *
-	 * @return void
-	 */
-	protected function mapConsoleRoutes()
-	{
 		\Artisan::command('phpcs', function () {
 			CodequalityProvider::phpcsCheck();
 		})->describe('Check syntax errors via PHPCS');
-		
+
 		\Artisan::command('phpcs:fix', function () {
 			CodequalityProvider::phpcsFix();
 		})->describe('Fix syntax errors via PHPCBF');
 	}
+
 	/**
 	 * PHPCS
 	 */
